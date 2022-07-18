e51fa9f587a8f04f5306c2e3df4c806247d8ad80
peterfalk2 <106140224+peterfalk2@users.noreply.github.com>
2022-07-18 15:03:27 +1000

    Assignment completed

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(num1, num2) {
+    return(num1 + num2)
+}
+
+function subtract(num1, num2) {
+    return(num1 - num2)
+}
+
+function multiply(num1, num2) {
+    return(num1 * num2)
+}
+
+function divide(num1, num2) {
+    return(num1 / num2)
+}
+
+function increment(n) {
+    n++
+    return n
+}
+
+function decrement(n) {
+    n--
+    return n
+}
+
+function makeInt(n) {
+    n = `a.toString`
+    parseInt(n)
+    return a
+}
+
+function makeInt(n) {
+    n = `0x2328`
+    parseInt(n, 10)
+    return 0
+}
+
+function makeInt(n) {
+   return  parseInt(n, 10)   
+}
+
+function preserveDecimal(n) {
+   return parseFloat(n, 10)
+}
+
+ 
+
+ 
+
+
+    
